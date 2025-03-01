fc7af40bf01052910a5fe69517d4da44c89b0ea4
rmf11048 <132789882+rmf11048@users.noreply.github.com>
2025-03-01 08:59:18 +0000

    Update AndroidLocationProviderClient.kt
    AndroidProvider to use GPS location only

--- android/src/main/kotlin/yukams/app/background_locator_2/provider/AndroidLocationProviderClient.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/provider/AndroidLocationProviderClient.kt
@@ -24,7 +24,9 @@ class AndroidLocationProviderClient(context: Context, override var listener: Loc
     @SuppressLint("MissingPermission")
     override fun requestLocationUpdates(request: LocationRequestOptions) {
         var gpsLocation: Location? = null
-        var networkLocation: Location? = null
+        
+        //var networkLocation: Location? = null
+        
         timeBetweenLocation = request.interval
         if (client?.isProviderEnabled(LocationManager.GPS_PROVIDER) == true) {
             client.requestLocationUpdates(LocationManager.GPS_PROVIDER,
@@ -32,26 +34,34 @@ class AndroidLocationProviderClient(context: Context, override var listener: Loc
                     request.distanceFilter,
                     this)
         }
+        
+        /*
         if (client?.isProviderEnabled(LocationManager.NETWORK_PROVIDER) == true) {
             client.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
                     request.interval,
                     request.distanceFilter,
                     this)
         }
+        */
+        
         gpsLocation = client?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-        networkLocation = client?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-        // return the android device last Location after start request location
-        if (gpsLocation != null && networkLocation != null) {
+        // networkLocation = client?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        
+        /*if (gpsLocation != null && networkLocation != null) {
             if (gpsLocation.time < networkLocation.time) {
                 onLocationChanged(networkLocation)
             } else {
                 onLocationChanged(gpsLocation)
             }
-        } else if (gpsLocation != null) {
+        } else */
+        
+        // return the android device last Location after start request location
+        if (gpsLocation != null) {
             onLocationChanged(gpsLocation)
-        } else if (networkLocation != null) {
+        } 
+        /*else if (networkLocation != null) {
             onLocationChanged(networkLocation)
-        }
+        } */
     }
 
     override fun onLocationChanged(location: Location) {
